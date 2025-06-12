dfe5278529e580f62deb7531feef2d9848785cac
Steffen Balmer <steffeb.balmer@futuretrainings.com>
2025-06-12 12:34:21 +0200

    New Title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React mit Datenbank
+          Learn React mit Herrn Ates
         </a>
       </header>
     </div>
