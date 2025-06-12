029d4b36d9c4f1f56dba26678fd477a435f758ce
Steffen Balmer <steffeb.balmer@futuretrainings.com>
2025-06-12 12:46:53 +0200

    Titel geändert

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React mit Herrn Ates
+          Learn React am Donnerstag
         </a>
       </header>
     </div>
